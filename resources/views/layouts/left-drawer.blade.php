4faaa0ad4912f95c480ed2a0fcf5cf3f41f34fb0
roland marvelous <thekontinet@gmail.com>
2024-09-24 12:24:42 +0100

    ui refactoring

--- resources/views/layouts/left-drawer.blade.php
+++ resources/views/layouts/left-drawer.blade.php
@@ -74,6 +74,17 @@
                 </a>
             </li>
 
+            @if(auth()->user()->is_admin)
+            <li class="mb-4">
+                <a href="{{ route('filament.admin.pages.dashboard') }}" class="flex gap-2 items-center">
+                    <svg class="size-6" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"></path>
+                      </svg>
+                    Admin Dashboard
+                </a>
+            </li>
+            @endif
+
             <li class="mb-4">
                 <button class="flex gap-2 items-center" form="logout-form">
                     <svg class="size-6" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
