7a7f62494f8cf88d5608b199f4dc4440b49b4622
roland marvelous <thekontinet@gmail.com>
2024-10-19 05:02:10 +0100

    ui update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
 
             <!-- Page Heading -->
             @isset($header)
-                <header class="bg-white shadow dark:bg-gray-800">
+                <header class="shadow dark:bg-gray-800">
                     <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
