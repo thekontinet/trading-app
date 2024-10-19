ae46561624e04b36ab0f26a1ee52a42bd5e6ed80
roland marvelous <thekontinet@gmail.com>
2024-10-19 05:04:47 +0100

    ui update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <div class="flex justify-between items-center">
-            <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+            <h2 class="text-xl font-semibold leading-tight">
                 {{ __('Dashboard') }}
             </h2>
 
