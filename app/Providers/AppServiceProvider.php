2b410e566cdd5d2fe7136ff9644956b5463e601c
roland marvelous <thekontinet@gmail.com>
2024-11-07 20:38:05 +0100

    fixed migration issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(191);
+        Schema::defaultStringLength(1911);
         View::addNamespace('equity', resource_path('themes/equity'));
         Blade::anonymousComponentNamespace(resource_path('themes/equity/components'), 'equity');
     }
