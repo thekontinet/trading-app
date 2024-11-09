0dcc1415b0f32193d982fbd668d9b8664d95b1f8
roland marvelous <thekontinet@gmail.com>
2024-11-09 12:23:35 +0100

    updated testimony default row

--- app/Models/Testimony.php
+++ app/Models/Testimony.php
@@ -20,13 +20,13 @@ class Testimony extends Model
 
     protected $rows = [
         [
-            'image' => 'http://broker-app.test/themes/equity/img/blockit/in-team-1.png',
+            'image' => '/themes/equity/img/blockit/in-team-1.png',
             'title' => 'Angela Nannenhorn',
             'subtitle' => 'from United Kingdom',
             'content' => 'Very convenience for trader, spread for gold is relatively low compare to other broker',
         ],
         [
-            'image' => 'http://broker-app.test/themes/equity/img/blockit/in-team-8.png',
+            'image' => '/themes/equity/img/blockit/in-team-8.png',
             'title' => 'Wade Palmer',
             'subtitle' => 'from Germany',
             'content' => 'One of the best FX brokers, I have been using! their trading conditions are excellent',
